0e17b1b0d5957d3772f218567aa5ef7f9e7a7880
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-11-16 09:41:55 -0300

    321

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .correcao import corrigirRequerimento
 
 
 # Create your views here.
 
 class Index(TemplateView):
     template_name = 'core/index.html'
-
-    def get_context_data(self, **kwargs):
-        contexto = super().get_context_data(**kwargs)
-        corrigirRequerimento()
-        return contexto
